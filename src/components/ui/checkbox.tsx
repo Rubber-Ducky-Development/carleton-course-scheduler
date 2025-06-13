51b52c10a367406fe113cf5f981a56e54b5753c7
= <bud.druid@gmail.com>
2025-06-13 15:13:37 -0400

    Fix checkbox component to make the entire area clickable

--- src/components/ui/checkbox.tsx
+++ src/components/ui/checkbox.tsx
@@ -9,27 +9,27 @@ export interface CheckboxProps extends React.InputHTMLAttributes<HTMLInputElemen
 const Checkbox = React.forwardRef<HTMLInputElement, CheckboxProps>(
     ({ className, label, ...props }, ref) => {
         return (
-            <div className="flex items-center space-x-2">
-                <div className="relative flex items-center">
-                    <input
-                        type="checkbox"
-                        ref={ref}
-                        className={cn(
-                            'peer h-4 w-4 appearance-none rounded border border-gray-300 bg-white checked:border-primary-500 checked:bg-primary-500 focus:outline-none focus:ring-2 focus:ring-primary-500 focus:ring-offset-0 disabled:cursor-not-allowed disabled:opacity-50 dark:border-gray-700 dark:bg-gray-900',
-                            className
-                        )}
-                        {...props}
-                    />
-                    <CheckIcon className="absolute left-0 top-0 h-4 w-4 text-white opacity-0 peer-checked:opacity-100" />
-                </div>
-                {label && (
-                    <label
-                        htmlFor={props.id}
-                        className="text-sm font-medium text-gray-700 dark:text-gray-300"
-                    >
-                        {label}
-                    </label>
-                )}
+            <div>
+                <label className="flex items-center cursor-pointer">
+                    <div className="relative flex items-center">
+                        <input
+                            type="checkbox"
+                            ref={ref}
+                            className={cn(
+                                'peer h-4 w-4 cursor-pointer appearance-none rounded border border-gray-300 bg-white checked:border-primary-500 checked:bg-primary-500 focus:outline-none focus:ring-2 focus:ring-primary-500 focus:ring-offset-0 disabled:cursor-not-allowed disabled:opacity-50 dark:border-gray-700 dark:bg-gray-900',
+                                className
+                            )}
+                            id={props.id}
+                            {...props}
+                        />
+                        <CheckIcon className="absolute left-0 top-0 h-4 w-4 text-white opacity-0 pointer-events-none peer-checked:opacity-100" />
+                    </div>
+                    {label && (
+                        <span className="ml-2 text-sm font-medium text-gray-700 dark:text-gray-300">
+                            {label}
+                        </span>
+                    )}
+                </label>
             </div>
         );
     }
